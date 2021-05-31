dbc0aeca46560a2eefa78256143aec62b942d798
pnovata <83655075+pnovata@users.noreply.github.com>
2021-05-31 20:39:52 +0200

    Update Cancellatore Barison.bat

--- Cancellatore Barison.bat
+++ Cancellatore Barison.bat
@@ -8,16 +8,20 @@ echo #########################################
 set /p Nome_Utente= Inserisci il Nome Utente: 
 
 
+pause
 
-reg delete HKEY_CURRENT_USER\Software\Blizzard Entertainment\Battle.net\Identity /f
+reg delete "HKEY_CURRENT_USER\Software\Blizzard Entertainment"\ /f
 
-rmdir "C:\Users\%Nome_Utente%\AppData\Local\Blizzard Entertainment" /q /s
-rmdir "C:\Users\%Nome_Utente%\AppData\Local\Battle.net" /q /s
-rmdir "C:\Users\%Nome_Utente%\AppData\Roaming\Battle.net" /q /s
-rmdir "C:\ProgramData\Battle.net" /q /s
-rmdir "C:\ProgramData\Blizzard Entertainment" /q /s
-rmdir "C:\Users\%Nome_Utente%\Documents\Call of Duty Modern Warfare" /q /s
-rmdir "E:\Giochi\Call of Duty Modern Warfare\Data\data\shmem" /q /s
-rmdir "E:\Giochi\Call of Duty Modern Warfare\main\recipes\cmr_hist" /q /s
+taskkill /f /im Agent.exe
+taskkill /f /im Battle.net.exe
+taskkill /f /im Battle.net\ Helper.exe
+del "C:\Users\alber\AppData\Local\Blizzard Entertainment" /q /s
+del "C:\Users\alber\AppData\Local\Battle.net" /q /s
+del "C:\Users\alber\AppData\Roaming\Battle.net" /q /s
+del "C:\ProgramData\Battle.net" /q /s
+del "C:\ProgramData\Blizzard Entertainment" /q /s
+del "C:\Users\alber\Documents\Call of Duty Modern Warfare" /q /s
+del "E:\Giochi\Call of Duty Modern Warfare\Data\data\shmem" /q /s
+del "E:\Giochi\Call of Duty Modern Warfare\main\recipes\cmr_hist" /q /s
 
 pause
